879e53f467154b707c5014a0a6f07b5f5382560d
nasawz <nasa127@gmail.com>
2023-02-26 20:39:42 +0800

    feat: analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,23 @@
 import type { ReactElement } from 'react'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from "next-themes"
+import { Analytics } from '@vercel/analytics/react';
 
 import '../style.css'
 import { Toaster } from '@components/ui/toaster'
 
 export default function App({ Component, pageProps }: AppProps) {
-    return (
-      <>
-        <style jsx global>{`
+  return (
+    <>
+      <style jsx global>{`
                   :root {
                   }
               }`}</style>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <Component {...pageProps} />
-        </ThemeProvider>
-        <Toaster />
-      </>
-    )
-  }
-  
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <Component {...pageProps} />
+      </ThemeProvider>
+      <Toaster />
+      <Analytics />
+    </>
+  )
+}
